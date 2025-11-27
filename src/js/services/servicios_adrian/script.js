c9d3fc51a45a4df9827560905c0c404384f03f52
tilin <adririsco05@gmail.com>
2025-11-27 21:07:04 +0100

    Modificado el css para implementar js modificado de la vista registro.html para js y validaciones del registro hechas en el archivo script.js

--- src/js/services/servicios_adrian/script.js
+++ src/js/services/servicios_adrian/script.js
@@ -1,4 +1,4 @@
-//Validaciones Registro
+//VALIDACIONES REGISTRO
 
 //Recojo los datos del form
 let form=document.getElementById("registro");
@@ -6,23 +6,76 @@ let nombreUsuario=document.getElementById("nombreUsuario");
 let email=document.getElementById("email");
 let nick=document.getElementById("nick");
 
-//Creamos elemento style
-let style=document.createElement("style");
-
 
 //Cuando se pulsa el botón enviar...
 form.addEventListener("submit", function(e){
     e.preventDefault();                                                 //Evita que se envíe el formulario
-
-    let nombre=nombreUsuario.value.trim();                           //Quita los espacios extra
-    let correo=email.value.trim();
-    let nameUser=nick.value.trim();
-
-    if(nombre==""){
-        console.log("Nombre Usuario Vacío");
-        nombreUsuario.placeholder="Nombre Usuario Vacío";
-        nombreUsuario.style.color="red";
-    }else{
-        console.log(nombreUsuario.value);
-    }
 });
+
+//Cuando escribes en el input nombreUsuario
+nombreUsuario.addEventListener("input", () => {                              //En tiempo real detecta lo que escribes en el input
+    validarNombreUsuario();
+});
+
+//Cuando escribes en el input email
+email.addEventListener("input", () => {                              //En tiempo real detecta lo que escribes en el input
+    validarEmail();
+});
+
+//Cuando escribes en el input nick
+nick.addEventListener("input", () => {                              //En tiempo real detecta lo que escribes en el input
+    validarNick();
+});
+
+
+//Validar el Nombre de Usuario
+function validarNombreUsuario(){
+    let nombreUsuarioValor=nombreUsuario.value.trim();                  //La función trim() quita los espacios extra
+
+    if(nombreUsuarioValor===""){
+        validarError(nombreUsuario, "Nombre de Usuario Inválido");
+    }else{
+        console.log(nombreUsuarioValor);
+        validarOk(nombreUsuario);
+    }
+}
+
+//Validar el email
+function validarEmail(){
+    let emailValor=email.value.trim();
+
+    if(emailValor==="" || !email.checkValidity()){
+        validarError(email, "Correo Inválido");
+    }else{
+        console.log(emailValor);
+        validarOk(email);
+    }
+}
+
+//Validar el nick
+function validarNick(){
+    let nickValor=nick.value.trim();
+
+    if(nickValor===""){
+        validarError(nick, "Nick Inválido");
+    }else{
+        console.log(nickValor);
+        validarOk(nick);
+    }
+}
+
+function validarError(input, mensaje){
+    let controlForm=input.parentElement;                            //La propiedad parentElement devuelve el elemento padre
+    let aviso=controlForm.querySelector("p");                       //Elegimos el selector donde meteremos lo que devuelve
+    aviso.innerText=mensaje;                                        //Metemos en aviso el texto del mensaje
+
+    controlForm.className="control error";                          //Creamos la clase error
+}
+
+function validarOk(input){
+    let controlForm=input.parentElement;
+    let aviso=controlForm.querySelector("p");
+    aviso.innerText="";                                             //Para borrar el texto del p si tiene
+
+    controlForm.className="control correcto";                       //Creamos la clase correcto
+}
