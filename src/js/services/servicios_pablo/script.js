3d468c74e325c3ab348ba4afc600c25524feaa20
PabloP05 <pablopardorodriguez.guadalupe@alumnado.fundacionloyola.net>
2025-11-28 12:00:50 +0100

    inicio de sesion de administrador configurado con js

---
+++ src/js/services/servicios_pablo/script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    const nombreFallo = document.getElementById('nombreFallo');
+const pwFallo = document.getElementById('pwFallo');
+const nombre = document.getElementById('nombre');
+const pw = document.getElementById('pw');
+
+const inicio_de_sesicon = document.getElementById('inicio_de_sesicon');
+
+inicio_de_sesicon.addEventListener('submit',(e)=>{
+    if(nombre.value.trim()==""){
+        e.preventDefault();
+        nombreFallo.innerText = "introduzca en nombre de administrador"
+        nombre.style.border='1px solid red'
+    }
+})
+
+nombre.addEventListener('input',()=>{
+    nombre.style.border='1px solid green'
+    nombreFallo.innerText=''
+    if(nombre.value.trim()==""){
+         nombre.style.border='none'
+    }
+});
+});
