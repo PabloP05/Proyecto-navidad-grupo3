8188bb20ab499985df863ff82473bb8802951685
tilin <adririsco05@gmail.com>
2025-11-30 09:58:07 +0100

    Añadida la función aleatoria de los modos en scriptMenuPrincipal.html

--- src/js/services/servicios_adrian/scriptMenuPrincipal.js
+++ src/js/services/servicios_adrian/scriptMenuPrincipal.js
@@ -19,7 +19,21 @@ btnSala.addEventListener("click", () => {
 
 //Evento cuando pulsamos partida pública
 btnPartidaPublica.addEventListener("click", () => {
-    window.location.href="../../../../../diseño/mockup/mockup_adrian/html/vistas-juegos/preguntasRespuestas.html";
+    //Iniciar Juego
+
+    let modos=["modo1","modo2","modo3"];
+
+    /*Para sacar el modo aleatorio usamos el Math.random() que eso devyelve 0 y 1, lo multiplicamos por el tamaño del array modos
+    y la función Math.floor redondea el resultado a un número entero, (0.14=0)*/
+    let modoAleatorio=modos[Math.floor(Math.random()*modos.length)];
+
+    if(modoAleatorio=="modo1"){
+        window.location.href="../../../../../diseño/mockup/mockup_adrian/html/vistas-juegos/preguntasRespuestas.html";
+    }else if(modoAleatorio=="modo2"){
+        window.location.href="../../../../../diseño/mockup/mockup_adrian/html/vistas-juegos/situaciones.html";
+    }else{
+        window.location.href="../../../../../diseño/mockup/mockup_adrian/html/vistas-juegos/fotos.html";
+    }
 });
 
 //Evento cuando pulsamos ranking
