a8c3ad691400f94f59d02c9e185b6b42fd2c65c4
PabloP05 <pablopardorodriguez.guadalupe@alumnado.fundacionloyola.net>
2025-12-04 19:45:34 +0100

    saneado a la version correcta

--- src/php/config/indexConfig.php
+++
@@ -1,9 +0,0 @@
-<?php
-    define('RUTA_MODELO','../model/');
-    define('RUTA_CONTROLADOR','controller/');
-    define('RUTA_VISTAS','views/');
-
-    define('C_DEFECTO','C_inicio');
-    // Método por defecto: debe ser el nombre del método, SIN paréntesis
-    define('M_DEFECTO', 'vistaInicial');
-?>
